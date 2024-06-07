871d6aecaefadbd4dfd384ba3bd1ee59ec86aa55
Ritik Saxena <ritiksaxena124@gmail.com>
2024-06-08 00:01:37 +0530

    signin and blog input validation added
    Signed-off-by: Ritik Saxena <ritiksaxena124@gmail.com>

--- Week 13/medium/common/src/index.ts
+++ Week 13/medium/common/src/index.ts
@@ -5,5 +5,18 @@ export const signupInput = z.object({
   password: z.string(),
 });
 
+export const signinInput = z.object({
+  email: z.string().email(),
+  password: z.string(),
+});
+
+export const blogInput = z.object({
+  title: z.string(),
+  content: z.string(),
+  published: z.boolean().optional(),
+});
+
 // Type inference
 export type SignupInput = z.infer<typeof signupInput>;
+export type SigninInput = z.infer<typeof signinInput>;
+export type BlogInput = z.infer<typeof blogInput>;
